f06efebbcb591dedab01f08ba02d860146a5d17c
Jaiganesh Girinathan <ganeshji@amazon.com>
2023-01-07 09:13:23 -0700

    updates to README

--- cdk/deploy.sh
+++ cdk/deploy.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-export AWS_PROFILE="demoportal"
-export HOSTEDZONE_ID='Z07319953J62AZLS1J9E2'
-export DOMAIN_NAME='demos.edge.aws.dev'
-export MONITOR_DOMAIN_PREFIX="cfmon1"
-export ORG_UNIT_ID="r-rqdg"
-# export MONITOR_DOMAIN_PREFIX="multicdn"
-# deploy staging configuration for CloudFront.Currently only ResponseHeaderPolicies are created
+export AWS_PROFILE=""
+export HOSTEDZONE_ID=''
+export DOMAIN_NAME=''
+export MONITOR_DOMAIN_PREFIX=""
+export ORG_UNIT_ID=""
 export DEPLOY_STAGING="TRUE"
 export DEPLOY_MULTICDN="FALSE"
 DEPLOY_AWS_REGION="us-east-1"
