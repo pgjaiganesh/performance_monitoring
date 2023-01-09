c2e99c22bd8d72df6c86e2d9b18903fb200c30bf
Jaiganesh Girinathan <ganeshji@amazon.com>
2023-01-09 05:59:28 -0700

    updates to README

--- cdk/deploy.sh
+++ cdk/deploy.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 # the AWS CLI profile name to be used.
 export AWS_PROFILE=""
-# 
-export HOSTEDZONE_ID=''
+# export HOSTEDZONE_ID=''
 # export DOMAIN_NAME=''
 export MONITOR_DOMAIN_NAME=""
 # AWS Organization ID
 export ORG_UNIT_ID=""
 # Deploy staging configuration for Real User Measurements with Continuous Deployment feature 
-export DEPLOY_STAGING="TRUE"
+# export DEPLOY_STAGING="TRUE"
 # Deploy config to support multi-cdn
-export DEPLOY_MULTICDN="FALSE"
+# export DEPLOY_MULTICDN="FALSE"
 # AWS Region to deploy the solution
 export CDK_DEPLOY_REGION=""
 npx cdk bootstrap --profile $AWS_PROFILE
